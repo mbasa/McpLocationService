9e9a0ffb86a6f26bf0119308aa027f3bc28f0ee8
mbasa <mario.basa@gmail.com>
2025-05-18 23:28:49 +0900

    mcp client test to access server

--- src/test/java/org/mbasa/mcpLocation/McpLocationApplicationTests.java
+++ src/test/java/org/mbasa/mcpLocation/McpLocationApplicationTests.java
@@ -1,13 +1,42 @@
 package org.mbasa.mcpLocation;
 
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
+import java.util.Map;
 
-@SpringBootTest
+import io.modelcontextprotocol.client.McpClient;
+import io.modelcontextprotocol.client.transport.ServerParameters;
+import io.modelcontextprotocol.client.transport.StdioClientTransport;
+import io.modelcontextprotocol.spec.McpSchema.CallToolRequest;
+import io.modelcontextprotocol.spec.McpSchema.CallToolResult;
+import io.modelcontextprotocol.spec.McpSchema.ListToolsResult;
+
 class McpLocationApplicationTests {
 
-	// @Test
-	// void contextLoads() {
-	// }
+	public static void main(String[] args) {
+
+		var stdioParams = ServerParameters.builder("java")
+				.args("-jar", "./target/mcpLocation.jar")
+				.build();
+
+		var transport = new StdioClientTransport(stdioParams);
+		var client = McpClient.sync(transport).build();
+
+		client.initialize();
+
+		// List and demonstrate tools
+		ListToolsResult toolsList = client.listTools();
+		System.out.println("Available Tools = " + toolsList);
+
+		CallToolResult geocoderResult = client
+				.callTool(new CallToolRequest("geocodeAddress", Map.of("address",
+						"杉並区清水一丁目−三−十四")));
+		System.out.println("Geocoder Result: " + geocoderResult.toString());
+
+		CallToolResult revGeocoderResult = client
+				.callTool(new CallToolRequest("reverseGeocode", Map.of("latitude",
+						35.710788822, "longitude", 139.620139631)));
+		System.out.println("Reverse Geocoder Result: " + revGeocoderResult.toString());
+
+		client.closeGracefully();
+	}
 
 }
