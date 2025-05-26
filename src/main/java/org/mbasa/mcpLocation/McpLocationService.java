cc7eb203a33f7e05e01b53ab8689d6ffe3f011be
mbasa <mario.basa@gmail.com>
2025-05-26 16:35:10 +0900

    added drive time polygon search

--- src/main/java/org/mbasa/mcpLocation/McpLocationService.java
+++ src/main/java/org/mbasa/mcpLocation/McpLocationService.java
@@ -30,7 +30,12 @@ public class McpLocationService {
     }
 
     @JsonIgnoreProperties(ignoreUnknown = true)
-    public record DriveTimeParams(
+    public record DrivePolyParams(
+            double latitude, double longitude, double radius) {
+    }
+
+    @JsonIgnoreProperties(ignoreUnknown = true)
+    public record DriveDistParams(
             double source_lat, double source_lng, double target_lat, double target_lng) {
     }
 
@@ -66,7 +71,7 @@ public class McpLocationService {
     }
 
     @Tool(description = "Driving Distance in meters between two Latitude, Longitude coordinates")
-    public String drivingDistance(DriveTimeParams drp) {
+    public String drivingDistance(DriveDistParams drp) {
 
         DriveRoute dr = getRestClient().get().uri(
                 "/pgrServer/api/latlng/dijkstra?source_x={source_x}8&source_y={source_y}&target_x={target_x}&target_y={target_y}",
@@ -76,8 +81,19 @@ public class McpLocationService {
         return "{\"distance_in_meters\" : " + dr.properties().feat_length() + "}";
     }
 
+    @Tool(description = "Creates a Drive Time Polygon with a Latitude, Longitude coordinate parmaeter with a Radius in meters")
+    public String driveTimePolygon(DrivePolyParams drpp) {
+
+        String retval = getRestClient().get().uri(
+                "/pgrServer/api/latlng/drivingDistance?source_x={source_x}8&source_y={source_y}&radius={radius}",
+                drpp.longitude, drpp.latitude, drpp.radius)
+                .retrieve().body(String.class);
+
+        return retval;
+    }
+
     @Tool(description = "Shortest Path between two Latitude, Longitude coordinates")
-    public String shortestPath(DriveTimeParams drp) {
+    public String shortestPath(DriveDistParams drp) {
 
         String retval = getRestClient().get().uri(
                 "/pgrServer/api/latlng/dijkstra?source_x={source_x}8&source_y={source_y}&target_x={target_x}&target_y={target_y}",
@@ -91,7 +107,7 @@ public class McpLocationService {
         McpLocationService client = new McpLocationService();
         System.out.println(client.geocodeAddress("杉並区清水１−３−１４"));
         System.out.println(client.reverseGeocode(35.710788822, 139.620139631));
-        System.out.println(client.drivingDistance(new DriveTimeParams(35.689627, 139.691778,
+        System.out.println(client.drivingDistance(new DriveDistParams(35.689627, 139.691778,
                 35.608323, 140.105996)));
     }
 
