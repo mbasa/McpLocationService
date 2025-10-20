161d677a5a941cd46ebf39eb2303620ba78bcc8f
mbasa <mario.basa@gmail.com>
2025-10-20 23:00:41 +0900

    added webMVC access

---
+++ src/main/java/org/mbasa/mcpLocation/ServletInitializer.java
@@ -0,0 +1,11 @@
+package org.mbasa.mcpLocation;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(McpLocationApplication.class);
+    }
+}
