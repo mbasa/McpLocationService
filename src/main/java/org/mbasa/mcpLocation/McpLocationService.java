c080415a9e04d015eb2bf43e843aa25145ec25df
mbasa <mario.basa@gmail.com>
2026-05-21 22:30:12 +0900

    refactor tools to use flat parameters and fix URL typos
    - Replace record-based @Tool parameters with flat name=value params,
      each annotated with @ToolParam(description=) for LLM clarity
    - Fix missing @ToolParam on reverseGeocode longitude parameter
    - Remove stray literal '8' appended to source_x in pgrServer URLs
    - Suppress harmless Spring AI BeanPostProcessor startup warning

--- src/main/java/org/mbasa/mcpLocation/McpLocationService.java
+++ src/main/java/org/mbasa/mcpLocation/McpLocationService.java
@@ -47,108 +47,121 @@ public class McpLocationService {
                 }
         }
 
-        @Tool(description = "Japanese 500m Mesh Census data which will be searched from a Latitude, Longitude coordinate parmaeter with a Radius in meters")
+        @Tool(description = "Japanese 500m Mesh Census data which will be searched from a Latitude, Longitude coordinate parameter with a Radius in meters")
         public String meshCensusData(
-                        @ToolParam(description = "Latitude, Longitude coordinate parmaeter with a Radius in meters that will be used to search the Census Mesh Data") DrivePolyParams dPolyParams) {
+                        @ToolParam(description = "Latitude of the center point") double latitude,
+                        @ToolParam(description = "Longitude of the center point") double longitude,
+                        @ToolParam(description = "Search radius in meters") double radius) {
 
-                String geoJson = this.driveTimePolygon(dPolyParams);
+                String geoJson = this.driveTimePolygon(latitude, longitude, radius);
 
                 return getRestClient().get()
                                 .uri("/CensusService/service/census/mesh4?geoJson={geoJson}", geoJson)
                                 .retrieve()
                                 .body(String.class);
-
         }
 
-        @Tool(description = "POI data which will be searched from a Latitude, Longitude coordinate parmaeter with a Radius in meters")
+        @Tool(description = "POI data which will be searched from a Latitude, Longitude coordinate parameter with a Radius in meters")
         public String poiData(
-                        @ToolParam(description = "Latitude, Longitude coordinate parmaeter with a Radius in meters that will be used to search the POI Data") DrivePolyParams dPolyParams) {
+                        @ToolParam(description = "Latitude of the center point") double latitude,
+                        @ToolParam(description = "Longitude of the center point") double longitude,
+                        @ToolParam(description = "Search radius in meters") double radius) {
 
-                String geoJson = this.driveTimePolygon(dPolyParams);
+                String geoJson = this.driveTimePolygon(latitude, longitude, radius);
 
                 return getRestClient().get()
                                 .uri("/CensusService/service/poi?geoJson={geoJson}", geoJson)
                                 .retrieve()
                                 .body(String.class);
-
         }
 
-        @Tool(description = "People Flow (人流) data which will be searched from a Latitude, Longitude coordinate parmaeter with a Radius in meters")
+        @Tool(description = "People Flow (人流) data which will be searched from a Latitude, Longitude coordinate parameter with a Radius in meters")
         public String peopleFlowData(
-                        @ToolParam(description = "Latitude, Longitude coordinate parmaeter with a Radius in meters that will be used to search the People Flow Data") DrivePolyParams dPolyParams) {
+                        @ToolParam(description = "Latitude of the center point") double latitude,
+                        @ToolParam(description = "Longitude of the center point") double longitude,
+                        @ToolParam(description = "Search radius in meters") double radius) {
 
-                String geoJson = this.driveTimePolygon(dPolyParams);
+                String geoJson = this.driveTimePolygon(latitude, longitude, radius);
 
                 return getRestClient().get()
                                 .uri("/CensusService/service/people_flow?geoJson={geoJson}", geoJson)
                                 .retrieve()
                                 .body(String.class);
-
         }
 
         @Tool(description = "Geocode input Japanese address")
-        public String geocodeAddress(@ToolParam(description = "Japanese address to be geocoded") String address) {
+        public String geocodeAddress(
+                        @ToolParam(description = "Japanese address to geocode") String address) {
 
-                String retVal = getRestClient().get().uri("/geocoderService/service/geocode/json/{address}", address)
+                return getRestClient().get()
+                                .uri("/geocoderService/service/geocode/json/{address}", address)
                                 .retrieve()
                                 .body(String.class);
-
-                return retVal;
         }
 
         @Tool(description = "Reverse Geocode Latitude, Longitude coordinates")
         public String reverseGeocode(
-                        @ToolParam(description = "reverse geocde the latitude/longitude points") double latitude,
-                        double longitude) {
+                        @ToolParam(description = "Latitude coordinate to reverse geocode") double latitude,
+                        @ToolParam(description = "Longitude coordinate to reverse geocode") double longitude) {
 
-                String retVal = getRestClient().get()
+                return getRestClient().get()
                                 .uri("/geocoderService/service/reversegeocode/json/{lon},{lat}",
                                                 longitude, latitude)
                                 .retrieve()
                                 .body(String.class);
-
-                return retVal;
         }
 
-        @Tool(description = "Driving Distance in meters between two Latitude, Longitude coordinates")
-        public String drivingDistance(DriveDistParams drp) {
+        @Tool(description = "Driving distance in meters between two Latitude, Longitude coordinates")
+        public String drivingDistance(
+                        @ToolParam(description = "Latitude of the source/starting point") double source_lat,
+                        @ToolParam(description = "Longitude of the source/starting point") double source_lng,
+                        @ToolParam(description = "Latitude of the target/destination point") double target_lat,
+                        @ToolParam(description = "Longitude of the target/destination point") double target_lng) {
+
+                DriveDistParams drp = new DriveDistParams(source_lat, source_lng, target_lat, target_lng);
 
                 DriveRoute dr = getRestClient().get().uri(
-                                "/pgrServer/api/latlng/dijkstra?source_x={source_x}8&source_y={source_y}&target_x={target_x}&target_y={target_y}",
-                                drp.source_lng, drp.source_lat, drp.target_lng, drp.target_lat)
+                                "/pgrServer/api/latlng/dijkstra?source_x={source_x}&source_y={source_y}&target_x={target_x}&target_y={target_y}",
+                                drp.source_lng(), drp.source_lat(), drp.target_lng(), drp.target_lat())
                                 .retrieve().body(DriveRoute.class);
 
                 return "{\"distance_in_meters\" : " + dr.properties().feat_length() + "}";
         }
 
-        @Tool(description = "Creates a Drive Time Polygon with a Latitude, Longitude coordinate parmaeter with a Radius in meters")
-        public String driveTimePolygon(DrivePolyParams drpp) {
+        @Tool(description = "Creates a Drive Time Polygon with a Latitude, Longitude coordinate parameter with a Radius in meters")
+        public String driveTimePolygon(
+                        @ToolParam(description = "Latitude of the center point") double latitude,
+                        @ToolParam(description = "Longitude of the center point") double longitude,
+                        @ToolParam(description = "Search radius in meters") double radius) {
 
-                String retval = getRestClient().get().uri(
-                                "/pgrServer/api/latlng/drivingDistance?source_x={source_x}8&source_y={source_y}&radius={radius}",
-                                drpp.longitude, drpp.latitude, drpp.radius)
+                DrivePolyParams drpp = new DrivePolyParams(latitude, longitude, radius);
+
+                return getRestClient().get().uri(
+                                "/pgrServer/api/latlng/drivingDistance?source_x={source_x}&source_y={source_y}&radius={radius}",
+                                drpp.longitude(), drpp.latitude(), drpp.radius())
                                 .retrieve().body(String.class);
-
-                return retval;
         }
 
-        @Tool(description = "Shortest Path between two Latitude, Longitude coordinates")
-        public String shortestPath(DriveDistParams drp) {
+        @Tool(description = "Shortest path between two Latitude, Longitude coordinates")
+        public String shortestPath(
+                        @ToolParam(description = "Latitude of the source/starting point") double source_lat,
+                        @ToolParam(description = "Longitude of the source/starting point") double source_lng,
+                        @ToolParam(description = "Latitude of the target/destination point") double target_lat,
+                        @ToolParam(description = "Longitude of the target/destination point") double target_lng) {
 
-                String retval = getRestClient().get().uri(
-                                "/pgrServer/api/latlng/dijkstra?source_x={source_x}8&source_y={source_y}&target_x={target_x}&target_y={target_y}",
-                                drp.source_lng, drp.source_lat, drp.target_lng, drp.target_lat)
+                DriveDistParams drp = new DriveDistParams(source_lat, source_lng, target_lat, target_lng);
+
+                return getRestClient().get().uri(
+                                "/pgrServer/api/latlng/dijkstra?source_x={source_x}&source_y={source_y}&target_x={target_x}&target_y={target_y}",
+                                drp.source_lng(), drp.source_lat(), drp.target_lng(), drp.target_lat())
                                 .retrieve().body(String.class);
-
-                return retval;
         }
 
         public static void main(String[] args) {
                 McpLocationService client = new McpLocationService();
                 System.out.println(client.geocodeAddress("杉並区清水１−３−１４"));
                 System.out.println(client.reverseGeocode(35.710788822, 139.620139631));
-                System.out.println(client.drivingDistance(new DriveDistParams(35.689627, 139.691778,
-                                35.608323, 140.105996)));
+                System.out.println(client.drivingDistance(35.689627, 139.691778, 35.608323, 140.105996));
         }
 
 }
